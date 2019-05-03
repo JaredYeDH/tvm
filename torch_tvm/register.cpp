ec89f330af55e2089c949cde1c54ba8351fe3094
Bram Wasti <bwasti@fb.com>
2019-05-02 17:31:03 -0700

    [WIP] Use PackedFunc to enable custom TVM operator registration from python
    gh-metadata: pytorch tvm 24 gh/bwasti/12/head

--- torch_tvm/register.cpp
+++ torch_tvm/register.cpp
@@ -6,6 +6,9 @@
 #include <torch/csrc/jit/pass_manager.h>
 #include <torch/csrc/jit/passes/graph_fuser.h>
 
+#include <tvm/runtime/packed_func.h>
+#include <tvm/runtime/registry.h>
+
 #include "compiler.h"
 #include "operators.h"
 
@@ -38,12 +41,45 @@ PYBIND11_MODULE(_torch_tvm, m) {
   });
 
   // python API to enable and disable tvm fusion
-  m.def("enable", [](){
-      setEnabled(true);
-  });
-  m.def("disable", [](){
-      setEnabled(false);
-  });
+  m.def("enable", []() { setEnabled(true); });
+  m.def("disable", []() { setEnabled(false); });
 
   m.doc() = "This module does nothing but register a TVM backend.";
 }
+
+TVM_REGISTER_GLOBAL("torch_tvm.register_operator")
+    .set_body([](tvm::runtime::TVMArgs args, tvm::runtime::TVMRetValue* rv) {
+      AT_CHECK(args.size() >= 2);
+      std::string name = args[0];
+      AT_ASSERT(args[1].type_code() == kFuncHandle);
+      tvm::PackedFunc* f =
+          new tvm::PackedFunc(args[1].operator tvm::PackedFunc());
+      auto& reg = tvm::relay::OpRegistry::Registry()
+                      ->__REGISTER_OR_GET__("torch." + name)
+                      .set_name();
+      reg.set_attr("FTVMCompute", *f, 10);
+
+      // tvm::Operation operation = args[1];
+      //.set_attr<FTVMCompute>("FTVMCompute",
+      // tvm::Expr expr = compute.body;
+      RegisterTVMOperator reg_op(
+          {{Symbol::fromQualString("tvm::" + name),
+            [name](Node* node, tvm::Array<tvm::relay::Expr> inputs) {
+              auto op = tvm::relay::Op::Get("torch." + name);
+              return tvm::relay::CallNode::make(op, inputs, tvm::Attrs(), {});
+            }}});
+
+      auto sym = Symbol::fromQualString("tvm::" + name);
+      auto options = OperatorOptions().aliasAnalysis(AliasAnalysisKind::PURE);
+      // TODO use function_schema constructor and extract num return args from
+      // *f()
+      RegisterOperators op({Operator(
+          sym,
+          [](const Node* node) {
+            return [](Stack& stack) {
+              AT_CHECK(0, "TODO")
+              return 0;
+            };
+          },
+          options)});
+    });
